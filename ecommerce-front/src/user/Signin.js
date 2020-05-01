0b328e940a112c6c71080f0f39154b22e39431fb
Dano <danielaherniv@gmail.com>
2020-05-01 17:39:44 -0400

    update

--- ecommerce-front/src/user/Signin.js
+++ ecommerce-front/src/user/Signin.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
-import { signin } from '../auth';
+import { signin, authenticate } from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: '',
-    password: '',
+    email: 'bob1@gmail.com',
+    password: '123456',
     error: '',
     loading: false,
     redirectToReferrer: false,
@@ -25,9 +25,11 @@ const Signin = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, loading: false });
       } else {
-        setValues({
-          ...values,
-          redirectToReferrer: true,
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true,
+          });
         });
       }
     });
@@ -83,8 +85,8 @@ const Signin = () => {
 
   return (
     <Layout
-      title='Signup'
-      description='Signup for Node React E-commerce App'
+      title='Signin'
+      description='Signin to Node React E-commerce App'
       className='container col-md-8 offset-md-2'
     >
       {showLoading()}
