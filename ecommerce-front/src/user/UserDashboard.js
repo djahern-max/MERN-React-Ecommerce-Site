c30728e8bb9a45317b8828c4d04a143cf003a767
Dano <danielaherniv@gmail.com>
2020-05-02 09:17:12 -0400

    created link for user dashboard

--- ecommerce-front/src/user/UserDashboard.js
+++ ecommerce-front/src/user/UserDashboard.js
@@ -3,6 +3,10 @@ import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 
 const Dashboard = () => {
+  const {
+    user: { _id, name, email, role },
+  } = isAuthenticated();
+
   return (
     <Layout
       title='Dashboard'
@@ -12,15 +16,17 @@ const Dashboard = () => {
       <div className='card mb-5'>
         <h3 className='card-header'>User Information</h3>
         <ul className='list-group'>
-          <li className='list-group-item'>name</li>
-          <li className='list-group-item'>email</li>
-          <li className='list-group-item'>role</li>
+          <li className='list-group-item'>{name}</li>
+          <li className='list-group-item'>{email}</li>
+          <li className='list-group-item'>
+            {role === 1 ? 'Admin' : 'Registered User'}
+          </li>
         </ul>
       </div>
       <div className='card mb-5'>
         <h3 className='card-header'>Purchase History</h3>
         <ul className='list-group'>
-          <li className='list-group-item'>history</li>
+          <li className='list-group-item'>History</li>
         </ul>
       </div>
     </Layout>
