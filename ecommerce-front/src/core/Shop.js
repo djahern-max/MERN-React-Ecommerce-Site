9b7e37b70703a6b580430a0de958ec9361d181ba
Dano <danielaherniv@gmail.com>
2020-05-10 17:22:47 -0400

    displaying prices

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -3,6 +3,8 @@ import Layout from './Layout';
 import Card from './Card';
 import { getCategories } from './apiCore';
 import Checkbox from './Checkbox';
+import RadioBox from './RadioBox';
+import { prices } from './fixedPrices';
 
 const Shop = () => {
   const [myFilters, setMyFilters] = useState({
@@ -48,6 +50,14 @@ const Shop = () => {
               handleFilters={(filters) => handleFilters(filters, 'category')}
             />
           </ul>
+
+          <h4>Filter by price range</h4>
+          <div>
+            <RadioBox
+              prices={prices}
+              handleFilters={(filters) => handleFilters(filters, 'price')}
+            />
+          </div>
         </div>
 
         <div className='col'>{JSON.stringify(myFilters)}</div>
