44cbea6b19bc39dc83b906b41f93e9943f2cba8b
Dano <danielaherniv@gmail.com>
2020-05-14 23:20:11 -0400

    finished search functionality

--- ecommerce-front/src/core/Search.js
+++ ecommerce-front/src/core/Search.js
@@ -49,12 +49,25 @@ const Search = () => {
     setData({ ...data, [name]: event.target.value, searched: false });
   };
 
+  const searchMessage = (searched, results) => {
+    if (searched && results.length > 0) {
+      return `Found ${results.length} products`;
+    }
+
+    if (searched && results.length < 1) {
+      return `No Products Found`;
+    }
+  };
+
   const searchedProducts = (results = []) => {
     return (
-      <div className='row'>
-        {results.map((product, i) => (
-          <Card key={i} product={product} />
-        ))}
+      <div>
+        <h2 className='mt-4 mb-4'>{searchMessage(searched, results)}</h2>
+        <div className='row'>
+          {results.map((product, i) => (
+            <Card key={i} product={product} />
+          ))}
+        </div>
       </div>
     );
   };
