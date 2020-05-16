6337f2a16d70f57f8943647c1fdc11c64eff3fdd
Dano <danielaherniv@gmail.com>
2020-05-16 18:00:49 -0400

    done check out Braintree next

---
+++ ecommerce-front/src/core/Checkout.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
+import { isAuthenticated } from '../auth';
+import { Link } from 'react-router-dom';
+
+const Checkout = ({ products }) => {
+  const getTotal = () => {
+    return products.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+
+  const showCheckout = () => {
+    return isAuthenticated() ? (
+      <button className='btn btn-success'>Checkout</button>
+    ) : (
+      <Link to='/signin'>
+        <button className='btn btn-primary'>Sign in to Checkout</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h4>Total: ${getTotal()}</h4>
+
+      {showCheckout()}
+    </div>
+  );
+};
+
+export default Checkout;
