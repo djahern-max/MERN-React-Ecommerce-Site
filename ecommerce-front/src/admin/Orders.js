851b15b031748394a44f919a1d28f0ac79a78249
Dano <danielaherniv@gmail.com>
2020-05-18 22:59:45 -0400

    updating order status

--- ecommerce-front/src/admin/Orders.js
+++ ecommerce-front/src/admin/Orders.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
-  // const [statusValues, setStatusValues] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
 
   const { user, token } = isAuthenticated();
 
@@ -21,6 +21,16 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
     // loadStatusValues();
@@ -29,12 +39,52 @@ const Orders = () => {
   const showOrdersLength = () => {
     if (orders.length > 0) {
       return (
-        <h1 className='text-danger display-2'>Total orders: {orders.length}</h1>
+        <h3 className='text-danger display-2'>Total orders: {orders.length}</h3>
       );
     } else {
-      return <h1 className='text-danger'>No orders</h1>;
+      return <h3 className='text-danger'>No orders</h3>;
     }
   };
+
+  const showInput = (key, value) => (
+    <div className='input-group mb-2 mr-sm-2'>
+      <div className='input-group-prepend'>
+        <div className='input-group-text'>{key}</div>
+      </div>
+      <input type='text' value={value} className='form-control' readOnly />
+    </div>
+  );
+
+  // const handleStatusChange = (e, orderId) => {
+  //   updateOrderStatus(user._id, token, orderId, e.target.value).then((data) => {
+  //     if (data.error) {
+  //       console.log('Status update failed');
+  //     } else {
+  //       loadOrders();
+  //     }
+  //   });
+  // };
+
+  const handleStatusChange = () => {
+    console.log('update order status');
+  };
+
+  const showStatus = (o) => (
+    <div className='form-group'>
+      <h3 className='mark mb-4'>Status: {o.status}</h3>
+      <select
+        className='form-control'
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>Update Status</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 
   return (
     <Layout
@@ -55,7 +105,7 @@ const Orders = () => {
                   <span className='bg-primary'>Order ID: {o._id}</span>
                 </h2>
                 <ul className='list-group mb-2'>
-                  <li className='list-group-item'>{o.status}</li>
+                  <li className='list-group-item'>{showStatus(o)}</li>
                   <li className='list-group-item'>
                     Transaction ID: {o.transaction_id}
                   </li>
@@ -73,6 +123,21 @@ const Orders = () => {
                 <h3 className='mt-4 mb-4 font-italic'>
                   Total products in the order: {o.products.length}
                 </h3>
+                {o.products.map((p, pIndex) => (
+                  <div
+                    className='mb-4'
+                    key={pIndex}
+                    style={{
+                      padding: '20px',
+                      border: '1px solid indigo',
+                    }}
+                  >
+                    {showInput('Product name', p.name)}
+                    {showInput('Product price', p.price)}
+                    {showInput('Product total', p.count)}
+                    {showInput('Product Id', p._id)}
+                  </div>
+                ))}
               </div>
             );
           })}
