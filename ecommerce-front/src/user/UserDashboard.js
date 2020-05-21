12557b8f160b95b1f26e9b688895930b8080d114
Dano <danielaherniv@gmail.com>
2020-05-20 23:15:41 -0400

    working on displaying purchase history

--- ecommerce-front/src/user/UserDashboard.js
+++ ecommerce-front/src/user/UserDashboard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { getPurchaseHistory } from './apiUser';
+import moment from 'moment';
 
 const Dashboard = () => {
+  const [history, setHistory] = useState([]);
   const {
     user: { _id, name, email, role },
   } = isAuthenticated();
+
+  const token = isAuthenticated().token;
+
+  const init = (userId, token) => {
+    getPurchaseHistory(userId, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setHistory(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(_id, token);
+  }, []);
 
   const userLinks = () => {
     return (
@@ -20,7 +39,7 @@ const Dashboard = () => {
           </li>
 
           <li className='list-group-item'>
-            <Link className='nav-link' to='/profile/update'>
+            <Link className='nav-link' to={`/profile/${_id}`}>
               Update Profile
             </Link>
           </li>
@@ -44,12 +63,12 @@ const Dashboard = () => {
     );
   };
 
-  const purchaseHistory = () => {
+  const purchaseHistory = (history) => {
     return (
       <div className='card mb-5'>
         <h3 className='card-header'>Purchase History</h3>
         <ul className='list-group'>
-          <li className='list-group-item'>History</li>
+          <li className='list-group-item'>{JSON.stringify(history)}</li>
         </ul>
       </div>
     );
