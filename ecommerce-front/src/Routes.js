247e620791540094d9996fe43a5abb4031e81e92
Dano <danielaherniv@gmail.com>
2020-05-21 22:54:49 -0400

    finished course ready to deploy

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -15,6 +15,8 @@ import Cart from './core/Cart';
 import Orders from './admin/Orders';
 import Profile from './user/Profile';
 import ManageProducts from './admin/ManageProducts';
+import UpdateProduct from './admin/UpdateProduct';
+import UpdateCategory from './admin/UpdateCategory';
 
 const Routes = () => {
   return (
@@ -33,6 +35,16 @@ const Routes = () => {
         <AdminRoute path='/admin/orders' exact component={Orders} />
         <PrivateRoute path='/profile/:userId' exact component={Profile} />
         <PrivateRoute path='/admin/products' exact component={ManageProducts} />
+        <AdminRoute
+          path='/admin/product/update/:productId'
+          exact
+          component={UpdateProduct}
+        />
+        <AdminRoute
+          path='/admin/category/update/:categoryId'
+          exact
+          component={UpdateCategory}
+        />
       </Switch>
     </BrowserRouter>
   );
